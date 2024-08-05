4fd990e0d768657a48c8c53068bf41178cd26da1
Tushar Chauhan <tusharchauhan069@protonmail.com>
2024-08-05 12:02:39 +0530

    reorganized for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import Navigation from './components/navigation/Navigation'
-import Carousel from './components/UI/Carousel';
-import HeroSection from './components/UI/HeroSection';
-import Cards from './components/layouts/Cards';
-import Milestone from './components/layouts/Milestone';
-import Reviews from './components/layouts/Reviews';
-import Results from './components/layouts/Results';
-import Footer from './components/footer/footer';
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <>
-      <Navigation />
-      <Carousel />
-      <HeroSection />
-      <Cards />
-      <Milestone />
-      <Reviews />
-      <Results />
-      <Footer />
+      <Home />
     </>
   );
 }
